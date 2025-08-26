160f04578b3fa7ea0dbb82c5603c3768df1af902
yhyunpy <yhyun.py@gmail.com>
2025-08-26 09:28:54 +0900

    feat: algo 14940

---
+++ algorithm/solved.ac/class3/14940.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def main():
+    queue = deque()
+
+    def bfs():
+        while queue:
+            now_x, now_y = queue.popleft()
+            for dx, dy in move:
+                x = now_x + dx
+                y = now_y + dy
+                if 0 <= x < N and 0 <= y < M:
+                    if graph[x][y] == -1:
+                        graph[x][y] = graph[now_x][now_y] + 1
+                        queue.append((x, y))
+
+
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 2:
+                graph[i][j] = 0
+                queue.append((i, j))
+            # not visiting 1과 0 다음에 도착한 칸의 1을 구분할 수 없으므로 -1로 not visiting를 표시
+            if graph[i][j] == 1:
+                graph[i][j] = -1
+
+    bfs()
+
+    for i in range(N):
+        temp = ""
+        for j in range(M):
+            temp += str(graph[i][j])
+            temp += " "
+        print(temp)
+
+if __name__ == "__main__":
+    main()
